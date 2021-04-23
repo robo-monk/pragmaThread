37fd3552c94977fb8d93ab603d2b082e57001626
robo-monk <rrobomonk@gmail.com>
2021-04-23 04:41:17 +0200

    update demo

--- docs/pragmaThread_demo.js
+++ docs/pragmaThread_demo.js
@@ -16,14 +16,39 @@ let thr = _thread()
                 .define(fib)
 
 
+let display = _p().as(_e('#display'))
+                .define(
+                    function clear() {
+                        this.html(' ')
+                        return this
+                    },
+                    function show(msg) {
+                        this.html(msg)
+                        return this
+                    }
+
+                )
+
 _e('#main-thread').listenTo('click', () => {
-    _e("#result").html(fib(42))
+    display.show("running fibonacci(42) in main-thread...")
+
+    setTimeout(() => {
+        let start = performance.now()
+        let res = fib(42)
+
+        display.show(`[${performance.now() - start}ms] result from main: ` + res)
+    }, 50)
 })
 
 _e('#threaded').listenTo('click', () => {
-    thr.fib(42).then(resp => {
-        // console.timeEnd('fibonacci')
-        _e("#result").html(resp)
-        console.log(resp)
-    })
+    display.show("running fibonacci(42) threaded...")
+    setTimeout(() => {
+
+        let start = performance.now()
+        thr.fib(42).then(resp => {
+            // console.timeEnd('fibonacci')
+            display.show(`[${performance.now() - start }ms] result from thread: ` + resp)
+            console.log(resp)
+        })
+    }, 50)
 })
